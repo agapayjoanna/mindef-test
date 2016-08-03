34ae8f992515777c8b621738ceb750b1c628d47e
Agapay <joanna.m.d.agapay@accenture.com>
2016-08-03 15:06:59 +0800

    [fix] loading spinner path update, official releases comment

--- WebContent/etc/design/imindef_style/imindef2012/custom.js
+++ WebContent/etc/design/imindef_style/imindef2012/custom.js
@@ -32,3 +32,17 @@ $('#tab_official_news-3 .officialslides').find(".activities-desc").dotdotdot({
 });	
 $('#tab_official_news-2').attr('style', 'display: none;');
 $('#tab_official_news-3').attr('style', 'display: none;');
+
+// Removed due to this issue: 
+// When Ctrl + click is performed to title, a new tab is opened and at the same time, the current tab url is changed to http://192.168.2.72/imindef/details.html?title=undefined&date=undefined
+/*
+$(document).on('click','#tab_official_news-1 .officialslides a',function(){
+	var title = $(this).data('title');
+	var date = $(this).data('date');
+	window.location.href="details.html?title="+title+"&date="+date;
+});
+$(document).on('click','#tab_official_news-2 .officialslides a',function(){
+	var title = $(this).data('title');
+	var date = $(this).data('date');
+	window.location.href="details.html?title="+title+"&date="+date;
+});	*/
